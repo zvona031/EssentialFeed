1c675d8ecebde16819ffa6744ad411899bea0c26
Zvonimir Pavlović <zvonimir.pavlovic@undabot.com>
2022-11-30 13:23:34 +0100

    FeedItem model added

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Zvonimir Pavlović on 30.11.2022..
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
