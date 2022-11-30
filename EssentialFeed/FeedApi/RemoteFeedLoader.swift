d4142e12687ebfe6eb426bdb76a2cc5fb1dc1763
Zvonimir Pavlović <zvonimir.pavlovic@undabot.com>
2022-11-30 14:20:27 +0100

    Remote feed loader moved to new folder

---
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Zvonimir Pavlović on 30.11.2022..
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+
+    public func load() {
+        client.get(from: url)
+    }
+}
