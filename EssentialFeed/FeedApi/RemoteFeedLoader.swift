98132c6aa09694a7506db186b5b3fe2c09b624bf
Zvonimir Pavlović <zvonimir.pavlovic@undabot.com>
2022-12-01 08:00:15 +0100

    Move codable logic into new private structure to decouple API details from FeedItem

--- EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -40,7 +40,8 @@ public final class RemoteFeedLoader {
             switch result {
             case let .success(data, response):
                 if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
+                    completion(.success(root.items.map({ $0.feedItem
+                    })))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -52,5 +53,16 @@ public final class RemoteFeedLoader {
 }
 
 private struct Root: Decodable {
-    let items: [FeedItem]
+    let items: [Item]
+}
+
+private struct Item: Decodable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let image: URL
+
+    var feedItem: FeedItem {
+        FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
 }
