5916f9443b1c66ad235704f79184aad1e07cbc48
Zvonimir Pavlović <zvonimir.pavlovic@undabot.com>
2023-02-28 18:19:29 +0100

    Added test for deleting cache upon saving new items

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -6,17 +6,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
-    let store: FeedStore
+    private let store: FeedStore
 
     init(store: FeedStore) {
         self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+
+    func deleteCachedFeed() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -28,4 +37,20 @@ final class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
     }
 
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+
+    private func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
 }
