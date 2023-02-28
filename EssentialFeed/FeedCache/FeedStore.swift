68e7e44f726575d65a626e8c2210a9252d159b88
Zvonimir Pavlović <zvonimir.pavlovic@undabot.com>
2023-02-28 20:04:12 +0100

    Move FeedStore to separate file

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Zvonimir Pavlović on 28.02.2023..
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
