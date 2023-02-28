631c442bc1d9765dfd78ba4aef365c4a37604673
Zvonimir Pavlović <zvonimir.pavlovic@undabot.com>
2023-02-28 20:39:01 +0100

    Added remote feed item representation to decouple api framework from FeedItem, extracted mapping to separate method

--- EssentialFeed/FeedApi/FeedItemMapper.swift
+++ EssentialFeed/FeedApi/FeedItemMapper.swift
@@ -7,13 +7,24 @@
 
 import Foundation
 
+struct RemoteFeedItem: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+
+    init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
+
 class FeedItemsMapper {
     private struct Root: Decodable {
-        let items: [Item]
-
-        var feed: [FeedItem] {
-            return items.map { $0.feedItem }
-        }
+        let items: [RemoteFeedItem]
     }
 
     private struct Item: Decodable {
@@ -29,12 +40,12 @@ class FeedItemsMapper {
 
     private static var OK_200: Int { 200 }
     
-    static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200,
               let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
         
-        return .success(root.feed)
+        return root.items
     }
 }
