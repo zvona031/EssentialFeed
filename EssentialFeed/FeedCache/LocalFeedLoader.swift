d85f686a3f722002384d899b9a08b54843ed7e5e
Zvonimir Pavlovic <zvona031@gmail.com>
2023-03-29 21:50:16 +0200

    Extract side effect on retrieval error

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -43,7 +43,6 @@ public final class LocalFeedLoader {
             guard let self else { return }
             switch result {
             case let .failure(error):
-                self.store.deleteCachedFeed(completion: { _ in })
                 completion(.failure(error))
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
@@ -54,6 +53,11 @@ public final class LocalFeedLoader {
                 completion(.success([]))
             }
         }
+    }
+    
+    public func validateCache() {
+        store.retrieve { _ in }
+        store.deleteCachedFeed { _ in }
     }
 
     private var maxCacheAgeInDays: Int {
