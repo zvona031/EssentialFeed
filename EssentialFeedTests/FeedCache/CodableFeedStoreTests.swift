32cc9e40b5fc189fd90005c940c936a5a6ae9310
Zvonimir Pavlovic <zvona031@gmail.com>
2023-04-02 18:18:32 +0200

    StoreURL injected

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -108,7 +108,8 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversFailureOnRetrievalError() {
-        let sut = makeSUT()
+        let storeURL = testSpecificStoreURL()
+        let sut = makeSUT(storeURL: storeURL)
 
         try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
 
@@ -117,9 +118,8 @@ final class CodableFeedStoreTests: XCTestCase {
     
     // MARK:- Helpers
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
-        let storeURL = testSpecificStoreURL()
-        let sut = CodableFeedStore(storeURL: storeURL)
+    private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
         trackForMemoryLeaks(sut)
         return sut
     }
