5ae08247df2af2962ebf42d25376bfe57d2a2785
Zvonimir Pavlovic <zvona031@gmail.com>
2023-05-28 13:10:39 +0200

    ci ios

---
+++ EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Zvonimir Pavlović on 28.05.2023..
+//
+
+import Foundation
--- EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import EssentialFeediOS
 
-final class EssentialFeediOSTests: XCTestCase {
+final class FeedViewControllerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
