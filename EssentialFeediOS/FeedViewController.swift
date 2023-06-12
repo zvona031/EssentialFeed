672a675529d5c88bf4229b18e954179084fb9b4e
Zvonimir Pavlovic <zvona031@gmail.com>
2023-06-12 22:26:22 +0200

    Does not alter current feed rendering state on load error

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -3,24 +3,44 @@ import EssentialFeed
 
 final public class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
-
+    private var tableModel = [FeedImage]()
+    
     public convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
     }
-
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
         load()
     }
-
+    
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
+        loader?.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.tableModel = feed
+                self?.tableView.reloadData()
+                self?.refreshControl?.endRefreshing()
+            case .failure: break
+            }
         }
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
     }
 }
