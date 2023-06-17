0a3e8672dc550eb451a30df9d71636993439410a
Zvonimir Pavlovic <zvona031@gmail.com>
2023-06-17 12:04:22 +0200

    Reuse table view cells from storyboard configuration

--- EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
+++ EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
@@ -1,21 +1,16 @@
 import UIKit
 
 public final class FeedImageCell: UITableViewCell {
-    public let locationContainer = UIView()
-    public let locationLabel = UILabel()
-    public let descriptionLabel = UILabel()
-    public let feedImageContainer = UIView()
-    public let feedImageView = UIImageView()
+    @IBOutlet private(set) public var locationContainer: UIView!
+    @IBOutlet private(set) public var locationLabel: UILabel!
+    @IBOutlet private(set) public var feedImageContainer: UIView!
+    @IBOutlet private(set) public var feedImageView: UIImageView!
+    @IBOutlet private(set) public var feedImageRetryButton: UIButton!
+    @IBOutlet private(set) public var descriptionLabel: UILabel!
+
+    var onRetry: (() -> Void)?
     
-    private(set) public lazy var feedImageRetryButton: UIButton = {
-            let button = UIButton()
-            button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
-            return button
-        }()
-
-        var onRetry: (() -> Void)?
-
-        @objc private func retryButtonTapped() {
-            onRetry?()
-        }
+    @IBAction private func retryButtonTapped() {
+        onRetry?()
+    }
 }
