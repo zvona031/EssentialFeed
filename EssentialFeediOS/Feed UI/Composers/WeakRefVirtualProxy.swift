f450871364ad5b3866dc5fe0ddd01887d9b6e992
Zvonimir Pavlovic <zvona031@gmail.com>
2023-06-17 17:17:38 +0200

    Replace FeedPresenter from iOS module with the FeedPresenter from the cross-platform EssentialFeed module

--- EssentialFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
+++ EssentialFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -1,10 +1,17 @@
 import UIKit
+import EssentialFeed
 
 final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
     
     init(_ object: T) {
         self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedErrorView where T: FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel) {
+        object?.display(viewModel)
     }
 }
 
