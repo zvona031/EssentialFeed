0eceec44e8491b0d95f870b2996cafb21b8b2a68
Zvonimir Pavlovic <zvona031@gmail.com>
2023-06-26 21:49:08 +0200

    Refactor procedural code into a chain of map operations

--- EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -4,17 +4,18 @@ extension CoreDataFeedStore: FeedImageDataStore {
     
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         perform { context in
-            guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
-            
-            image.data = data
-            try? context.save()
+            completion(Result {
+                try ManagedFeedImage.first(with: url, in: context)
+                    .map { $0.data = data }
+                    .map(context.save)
+            })
         }
     }
     
     public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         perform { context in
             completion(Result {
-                return try ManagedFeedImage.first(with: url, in: context)?.data
+                try ManagedFeedImage.first(with: url, in: context)?.data
             })
         }
     }
