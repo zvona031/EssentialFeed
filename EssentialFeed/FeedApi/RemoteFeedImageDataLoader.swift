357c5747bc3ea2e0995ca3f153a8295fbbdd1b6f
Zvonimir Pavlovic <zvona031@gmail.com>
2023-06-26 18:49:12 +0200

    Remove @discardableResult attribute — FeedImageDataLoaderTask instances should be properly managed and never discarded.

--- EssentialFeed/FeedApi/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/FeedApi/RemoteFeedImageDataLoader.swift
@@ -33,19 +33,18 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         }
     }
     
-    @discardableResult
-        public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-            let task = HTTPClientTaskWrapper(completion)
-            task.wrapped = client.get(from: url) { [weak self] result in
-                guard self != nil else { return }
-                
-                task.complete(with: result
-                    .mapError { _ in Error.connectivity }
-                    .flatMap { (data, response) in
-                        let isValidResponse = response.statusCode == 200 && !data.isEmpty
-                        return isValidResponse ? .success(data) : .failure(Error.invalidData)
-                    })
-            }
-            return task
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { (data, response) in
+                    let isValidResponse = response.statusCode == 200 && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                })
         }
+        return task
+    }
 }
