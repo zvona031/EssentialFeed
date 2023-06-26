28810cfb47ae1e5d7e160244405fb5274dea2727
Zvonimir Pavlovic <zvona031@gmail.com>
2023-06-26 18:46:42 +0200

    Refactor switch statement with map and flatMap chain

--- EssentialFeed/FeedApi/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/FeedApi/RemoteFeedImageDataLoader.swift
@@ -34,21 +34,18 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     }
     
     @discardableResult
-    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPClientTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success(data, response):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case let .failure(error): task.complete(with: .failure(Error.connectivity))
+        public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            let task = HTTPClientTaskWrapper(completion)
+            task.wrapped = client.get(from: url) { [weak self] result in
+                guard self != nil else { return }
+                
+                task.complete(with: result
+                    .mapError { _ in Error.connectivity }
+                    .flatMap { (data, response) in
+                        let isValidResponse = response.statusCode == 200 && !data.isEmpty
+                        return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                    })
             }
+            return task
         }
-        return task
-    }
 }
